1a03faeb700f12a6a76c7ade27de24819b2a1205
Forzalyonnais <verhaeghe.justin@yahoo.fr>
2023-09-11 11:10:24 +0200

    Split method MVC

---
+++ views/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Handjet:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Phudu&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>Validation Formulaire</title>
+</head>
+<!-- Header -->
+
+<body p-5>
+    <header class="d-flex flex-column justify-content-center align-items-center text-light">
+        <h1>Validation Formulaire</h1>
+    </header>
